b791fd9ce94594b8973050cffcfab41ec97caf5d
James Ji <jamesji.penfield@protonmail.com>
2021-07-23 18:19:29 -0400

    added data and cards to home

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,15 +1,47 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, Text } from "react-native";
+import { Card } from "react-native-elements";
+
+import { CAMPSITES } from "../shared/campsites";
+import { PROMOTIONS } from "../shared/promotions";
+import { PARTNERS } from "../shared/partners";
+
+const RenderItem = ({ item }) => {
+  if (item) {
+    return (
+      <Card>
+        <Card.Image source={require(`../assets/images/react-lake.jpg`)} />
+        <Card.Title>{item.name}</Card.Title>
+        <Text style={{ margin: 10 }}>{item.description}</Text>
+      </Card>
+    );
+  }
+  return <View />;
+};
 
 const Home = () => {
+  const [data, useData] = useState({
+    campsites: CAMPSITES,
+    promotions: PROMOTIONS,
+    partners: PARTNERS,
+  });
+
   const navigationOptions = {
     title: "Home",
   };
 
   return (
-    <View>
-      <Text>Home Component</Text>
-    </View>
+    <ScrollView>
+      <RenderItem
+        item={data.campsites.filter((campsite) => campsite.featured)[0]}
+      />
+         <RenderItem
+        item={data.promotions.filter((promotion) => promotion.featured)[0]}
+      />
+         <RenderItem
+        item={data.partners.filter((partner) => partner.featured)[0]}
+      />
+    </ScrollView>
   );
 };
 
