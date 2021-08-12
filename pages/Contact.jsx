7e0b31876d61ed8e596765f257fd68d69c8a747c
James Ji <jamesji.penfield@protonmail.com>
2021-08-12 16:01:02 -0400

    added send email option

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-import { Text, Card } from "react-native-elements";
+import { Text, Card, Button, Icon } from "react-native-elements";
 import * as Animatable from "react-native-animatable";
+
+import * as MailComposer from "expo-mail-composer";
+
+const sendMail = () => {
+  MailComposer.composeAsync({
+    recipients: ["campsites@nucamp.co"],
+    subject: "inquiry",
+    body: "to whom it may concern:",
+  });
+};
 
 const Contact = () => {
   return (
@@ -13,6 +23,19 @@ const Contact = () => {
         <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
         <Text>Phone: 1-206-555-1234</Text>
         <Text>Email: campsites@nucamp.co</Text>
+        <Button
+          title="Send Email"
+          buttonStyle={{ backgroundColor: "#5637DD", margin: 40 }}
+          icon={
+            <Icon
+              name="envelope-o"
+              type="font-awesome"
+              color="#FFF"
+              iconStyle={{ marginRight: 10 }}
+            />
+          }
+          onPress={() => sendMail()}
+        />
       </Card>
     </Animatable.View>
   );
