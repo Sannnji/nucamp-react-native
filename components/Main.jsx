856ea33014ae6be6a20d98db7b8dc3f4122ac180
Sannnji <jamesji.penfield@protonmail.com>
2021-08-14 16:17:23 -0400

    added netinfo to main component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,11 +1,52 @@
-import React from "react";
-import { View, Platform } from "react-native";
+import React, { useEffect } from "react";
+import { View, Platform, ToastAndroid, Alert } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Constants from "expo-constants";
+import NetInfo from "@react-native-community/netinfo";
 
 import MyDrawer from "./navigators/drawer/MyDrawer";
 
 const Main = () => {
+  useEffect(() => {
+    showNetInfo();
+
+    NetInfo.addEventListener((connectionInfo) => {
+      handleConnectivityChange(connectionInfo);
+    });
+  }, []);
+
+  async function showNetInfo() {
+    await NetInfo.fetch().then((connectionInfo) => {
+      Platform.OS === "ios"
+        ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
+        : ToastAndroid.show(
+            "Initial Network Connectivity Type: " + connectionInfo.type,
+            ToastAndroid.LONG
+          );
+    });
+  }
+
+  const handleConnectivityChange = (connectionInfo) => {
+    let connectionMsg = "You are now connected to an active network.";
+    switch (connectionInfo.type) {
+      case "none":
+        connectionMsg = "No network connection is active.";
+        break;
+      case "unknown":
+        connectionMsg = "The network connection state is now unknown.";
+        break;
+      case "cellular":
+        connectionMsg = "You are now connected to a cellular network.";
+        break;
+      case "wifi":
+        connectionMsg = "You are now connected to a WiFi network.";
+        break;
+    }
+    Platform.OS === "ios"
+      ? Alert.alert("Connection change:", connectionMsg)
+      : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+  };
+
   return (
     <View
       style={{
